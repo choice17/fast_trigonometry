226355dbb528e53b66c69b752f4c51646d2638ce
choice17 <choinus4ms@gmail.com>
2020-06-27 19:30:50 +0800

    Completion for lkup table

--- unit-test-print.py
+++ unit-test-print.py
@@ -11,24 +11,24 @@ import sys
 R = int(sys.argv[2])
 N = float(sys.argv[1])
 dat  = open("diff.bin", "rb").read()
-dat = np.fromstring(dat, np.float32)
-dat = dat.reshape(-1,4).T
+dat = np.frombuffer(dat, np.float32)
+name = ["atan", "sin", "cos", "tan", "acos", "asin"]
+dat = dat.reshape(-1,len(name)).T
 F,M = dat.shape
 logdiff = np.log2(dat)
-xaxis = np.arange(R) * 3.14 * N / R - 3.14 * N / 2
+xaxis = np.arange(R) * N / R - N / 2
 fig = plt.figure()
-ax = [0,0,0,0]
+ax = [0,0,0,0,0,0]
 MIN = -0.01
 MAX = 0.01
-name = ["atan", "sin", "cos", "tan"]
-for i in range(4):
-    ax[i]= fig.add_subplot(4,1,i+1)
+for i in range(len(name)):
+    ax[i]= fig.add_subplot(len(name),1,i+1)
     ax[i].plot(xaxis, dat[i])
     ax[i].set_ylabel(name[i])
     ax[i].axis(
         ymin=np.maximum(MIN,np.min(dat[i])),
         ymax=np.minimum(MAX, np.max(dat[i]))
         )
-ax[3].set_xlabel('angle(radian)')
+ax[-1].set_xlabel('angle(radian)')
 plt.show()
 
